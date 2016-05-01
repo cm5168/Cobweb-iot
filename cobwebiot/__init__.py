5876fe01f13701dcbde4e44505882e9f402174e6
Meng Cao <cm19920214@gmail.com>
2016-05-01 11:01:00 -0400

    compatibility

--- cobwebiot/__init__.py
+++ cobwebiot/__init__.py
@@ -1,5 +1,17 @@
-from . import client
-from . import server
-from . import beagle
-from . import rasp
-from . import dev
+from __future__ import print_function
+from cobwebiot import client
+from cobwebiot import server
+
+try:
+    from cobwebiot import beagle
+except:
+    print("No beaglebone imported")
+    pass
+
+try:
+    from cobwebiot import rasp
+except:
+    print("No Raspberry pi imported")
+    pass
+
+from cobwebiot import dev
